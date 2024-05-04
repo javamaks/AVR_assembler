99ab6aca8a717aa2afda54e38444364d8533507b
Жава <63841611+javamaks@users.noreply.github.com>
2024-05-04 09:04:01 +0500

    Add files via upload

--- main.asm
+++ main.asm
@@ -12,27 +12,18 @@ main:
     ldi r19, 0    
 
 button_check:
-    sbic PINB, PB7 ; Проверяем, нажата ли кнопка
-    rjmp button_check ; Если нет, ждем дальше
-
-    ; Кнопка нажата, увеличиваем счетчик
+    sbic PINB, PB7 
+    rjmp button_check 
     inc r19
-
-    ; Проверяем, если счетчик достиг 5, сбрасываем его
     cpi r19, 6
     breq reset_counter
-
-    ; Переходим к выбору сегмента
     rjmp segment_select
 
 reset_counter:
-    ; Сбрасываем счетчик
     ldi r19, 0
-    ; Переходим к задержке
     rjmp delay
 
 segment_select:
-    ; Выбор сегмента в зависимости от значения счетчика
     cpi r19, 1
     breq name1
     cpi r19, 2
@@ -43,46 +34,35 @@ segment_select:
     breq name4
     cpi r19, 5
     breq name5
-    ; Если счетчик не соответствует ни одному сегменту, переходим к началу проверки кнопки
     rjmp button_check
 
 
 name1:
-    ; Обнуляем регистры
     ldi r23, 0
-    ; Устанавливаем новое значение для порта B
     ldi r23, 0b1110111
     out PORTB, r23
     rjmp delay
 
 name2:
-    ; Обнуляем регистры
     ldi r24, 0
-    ; Устанавливаем новое значение для порта B
     ldi r24, 0b1010110
     out PORTB, r24
     rjmp delay
 
 name3:
-    ; Обнуляем регистры
     ldi r25, 0
-    ; Устанавливаем новое значение для порта B
     ldi r25, 0b1111001
     out PORTB, r25
     rjmp delay
 
 name4:
-    ; Обнуляем регистры
     ldi r26, 0
-    ; Устанавливаем новое значение для порта B
     ldi r26, 0b1110110
     out PORTB, r26
     rjmp delay
 
 name5:
-    ; Обнуляем регистры
     ldi r27, 0
-    ; Устанавливаем новое значение для порта B
     ldi r27, 0b1110111
     out PORTB, r27
     rjmp delay
